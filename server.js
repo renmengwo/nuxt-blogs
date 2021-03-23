c473a0d022a27bac1a1d680ab45a784dfd30a3f2
lh26297 <lh26297>
2021-03-23 16:02:23 +0800

    解決了post方法出現连接中断的問題

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const path = require('path');
 const express = require('express');
 const { Nuxt, Builder } = require('nuxt');
-const bodyParser = require('body-parser');
+
 const app = require('express')();
 const port = process.env.PORT || 3000;
 const indexRouter = require('./server/router/index');
 // We instantiate Nuxt.js with the options
 const config = require('./nuxt.config.js');
 const nuxt = new Nuxt(config);
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
 app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use('/', indexRouter);
 app.use(nuxt.render);
--- server/router/user.js
+++ server/router/user.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = new express.Router();
+const bodyParser = require('body-parser');
 const UserModelRouter = require('../controllers/user');
-router.post('/addUser', UserModelRouter.addUser);
+// parse application/x-www-form-urlencoded
+router.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+router.use(bodyParser.json());
 router.post('/login', UserModelRouter.login);
+router.post('/addUser', UserModelRouter.addUser);
 router.post('/getUserInfo', UserModelRouter.getUserInfo);
 
 module.exports = router;
